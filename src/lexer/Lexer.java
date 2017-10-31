ff033bec15ff0538a48c4f497578aa3f3f753538
puri8467 <puri8467@gmail.com>
2017-10-31 23:38:30 +0900

    lexer construct

---
+++ src/lexer/Lexer.java
@@ -0,0 +1,56 @@
+package lexer;
+
+import java.io.*;
+import token.*;
+
+public class Lexer {
+
+    private boolean isEof = false;
+    // 초기 값
+    private char ch = ' ';
+    // 버퍼
+    private BufferedReader input;
+    // 스트링
+    private String line = "";
+    //라인넘버
+    private int lineno = 0;
+    private int col = 1;
+    private final String letters = "abcdefghijklmnopqrstuvwxyz"
+        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String digits = "0123456789";
+    private final char eolnCh = '\n';   //줄바꿈
+    private final char eofCh = '\004';  //공백
+    
+
+    public Lexer (String fileName) { // source filename
+        try {
+            input = new BufferedReader (new FileReader(fileName));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+            System.exit(1);
+        }
+    }
+
+    private char nextChar() { // Return next char
+        if (ch == eofCh)
+            error("Attempt to read past end of file");
+        col++;
+        if (col >= line.length()) {
+            try {
+                line = input.readLine( );
+            } catch (IOException e) {
+                System.err.println(e);
+                System.exit(1);
+            } // try
+            if (line == null) // at end of file
+                line = "" + eofCh;
+            else {
+                // System.out.println(lineno + ":\t" + line);
+                lineno++;
+                line += eolnCh;
+            } // if line
+            col = 0;
+        } // if col
+        return line.charAt(col);
+    }
